2e918d09f20c61dcf28fe100164f06ab1aed5472
Jaavier8 <javierrodriguez987@gmail.com>
2023-09-04 12:38:45 +0200

    final changes

--- down.sh
+++ down.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker stop $(docker ps -a -q --filter ancestor=aries-cloudagent-run)
-docker stop $(docker ps -a -q --filter ancestor=ghcr.io/timoglastra/acapy-development-webhook-server)
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-tmux new-session -d -s vonnetwork '~/OneDrive/Escritorio/TFM/von-network/manage down'
-tmux new-session -d -s tailsserver '~/OneDrive/Escritorio/TFM/indy-tails-server/docker/manage down'
+docker stop $(docker ps -a -q --filter ancestor=aries-cloudagent-run)
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=ghcr.io/timoglastra/acapy-development-webhook-server))
+
+$SCRIPT_DIR/vonNetwork/manage down
+$SCRIPT_DIR/indyTailsServer/manage down
